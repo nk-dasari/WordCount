4f2eec49a5480547f8794ac29989cc1192ca97f8
nk-dasari <nareshdasari@outlook.com>
2017-08-16 11:42:26 -0700

    added reduce tasks#

--- src/main/java/com/hadoop/training/WordCountDriver.java
+++ src/main/java/com/hadoop/training/WordCountDriver.java
@@ -3,12 +3,14 @@ package com.hadoop.training;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-import com.sun.jersey.core.impl.provider.entity.XMLJAXBElementProvider.Text;
 
 public class WordCountDriver {
 
@@ -23,6 +25,9 @@ public class WordCountDriver {
 		job.setOutputValueClass(IntWritable.class);
 		job.setMapperClass(WordCountMapper.class);
 		job.setReducerClass(WordCountReducer.class);
+		job.setNumReduceTasks(1);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		job.waitForCompletion(true);
 	}
 
